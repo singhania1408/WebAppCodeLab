e95828edd3e971ba98bc721212024e7c0bd3e3cf
Abhishek Singhania <abhisinghania14@gmail.com>
2016-07-27 13:34:26 +0530

    Cities are storing and adding

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,35 @@
 
 (function() {
-  'use strict';
-  
+
+  'use strict'; 
+
+   var initialWeatherForecast = {
+    key: 'newyork',
+    label: 'New York, NY',
+    currently: {
+      time: 1453489481,
+      summary: 'Clear',
+      icon: 'partly-cloudy-day',
+      temperature: 52.74,
+      apparentTemperature: 74.34,
+      precipProbability: 0.20,
+      humidity: 0.77,
+      windBearing: 125,
+      windSpeed: 1.52
+    },
+    daily: {
+      data: [
+        {icon: 'clear-day', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'rain', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'snow', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'sleet', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'fog', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'wind', temperatureMax: 55, temperatureMin: 34},
+        {icon: 'partly-cloudy-day', temperatureMax: 55, temperatureMin: 34}
+      ]
+    }
+  };
+
 
   var app = {
     isLoading: true,
@@ -39,6 +67,7 @@
     var label = selected.textContent;
     app.getForecast(key, label);
     app.selectedCities.push({key: key, label: label});
+    app.saveSelectedCities();
     app.toggleAddDialog(false);
   });
 
@@ -176,5 +205,24 @@
     }
   };
   // Uncomment the line below to test with the provided fake data
-   app.updateForecastCard(fakeForecast);
+   //app.updateForecastCard(initialWeatherForecast);
+   app.saveSelectedCities = function() {
+       var selectedCities = JSON.stringify(app.selectedCities);
+       localStorage.selectedCities = selectedCities;
+     };
+
+      app.selectedCities = localStorage.selectedCities;
+  if (app.selectedCities) {
+    app.selectedCities = JSON.parse(app.selectedCities);
+    app.selectedCities.forEach(function(city) {
+      app.getForecast(city.key, city.label);
+    });
+  } else {
+    app.updateForecastCard(initialWeatherForecast);
+    app.selectedCities = [
+      {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+    ];
+    app.saveSelectedCities();
+  }
+
 })();
